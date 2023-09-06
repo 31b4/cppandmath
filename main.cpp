4846618d8839594aa3967290392ef0766461ec29
ben <75566095+31b4@users.noreply.github.com>
2023-09-06 23:38:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    std::cout <<"\t \t All Prime Number: \n";
+    int num = 1;
+    int i = 2;
+    bool prime = true;
+    while (true) {
+        while (prime) {
+            if (num % i == 0 && i <= num / 2) {
+                prime = false;
+            }
+            else if (i > num/2) {
+                std::cout<<num<<" ";
+                prime = false;
+            }
+            i++;
+        }
+        i = 2;
+        num++;
+        prime = true;
+    }    return 0;
+}

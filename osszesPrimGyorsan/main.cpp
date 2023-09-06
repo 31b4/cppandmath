b05e895bd65ffe62751f50efbf4230ba6e2f969c
ben <75566095+31b4@users.noreply.github.com>
2023-09-06 23:57:16 +0200

    2x gyorsabb lett :D

--- osszesPrimGyorsan/main.cpp
+++ osszesPrimGyorsan/main.cpp
@@ -2,7 +2,8 @@
 
 int main() {
     std::cout <<"\t \t All Prime Number: \n";
-    int num = 2;
+    std::cout<<"2 ";//kiirjuk hogy lehessen 3-tol kettesével haladni
+    int num = 3;
     int i = 2;
     bool prime = true;
     while (true) {
@@ -10,14 +11,14 @@ int main() {
             if (num % i == 0 && i <= num / 2) {
                 prime = false;
             }
-            else if (i > num/2) {
+            else if (i > num/2) {//szam felenel nagyobb oszto nincs
                 std::cout<<num<<" ";
                 prime = false;
             }
-            i++;
+            i+=2;//megneztuk hogy oszthato 2-vel szoval eleg ha csak a páratlan számokat nézzuk
         }
         i = 2;
-        num++;
+        num+=2;//csak paratlan primszamok vannak szoval 3-tol kettesével haladhatunk
         prime = true;
     }    return 0;
 }
